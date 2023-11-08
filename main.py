d2117828a8284e6f71f096d836fc4d831d1b2999
Ziqi Gong <79713887+zgong26@users.noreply.github.com>
2023-11-08 15:46:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,44 +8,6 @@ import torchvision.models as models
 import os
 from tqdm import tqdm
 
-# Custom Dataset
-"""
-class OvarianCancerDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.annotations = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
-        self.label_dict = {'CC': 0, 'EC': 1, 'LGSC': 2, 'HGSC': 3, 'MC': 4}
-
-    def __len__(self):
-        return len(self.annotations)
-
-    def __getitem__(self, index):
-        img_id = self.annotations.iloc[index, 0]
-        img_name = os.path.join(self.root_dir, f"{img_id}.jpg")
-        Image.MAX_IMAGE_PIXELS = None
-        image = Image.open(img_name).convert("RGB")
-        label = self.annotations.iloc[index, 1]
-        label = self.label_to_idx(label)  # Convert label to index
-
-        if self.transform:
-            image = self.transform(image)
-        print(f"img: {img_id}, {self.annotations.iloc[index, 1]}")
-        return image, label
-
-    def label_to_idx(self, label):
-        return self.label_dict[label]
-
-
-# Dataset and DataLoader
-
-dataset = OvarianCancerDataset(csv_file='train.csv',
-                               root_dir='train_images_compressed_80',
-                               transform=transform)
-
-train_loader = DataLoader(dataset=dataset, batch_size=128, shuffle=True)
-"""
-
 
 class OvarianCancerDataset(Dataset):
     def __init__(self, annotations_file, root_dir):
